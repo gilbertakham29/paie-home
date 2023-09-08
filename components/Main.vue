aa37b0d0a4ff7721d87d1c5641d88433b5194f24
Gilbert <akhamgilbert@gmail.com>
2023-09-08 12:02:29 +0530

    completed the paie-home UI using flowbite and nuxtjs

---
+++ components/Main.vue
@@ -0,0 +1,259 @@
+<template>
+   <div>
+  <div class="bg-[#FEF9EC]">
+    <div class="md:hidden">
+      <img src="https://paie-home-amt.netlify.app/_nuxt/dashboard.1d4bae62.png" alt="dashboard">
+    </div>
+    <section class="pb-5 md:pb-0">
+      <div class="pt-5 px-4 mx-auto max-w-screen-xl text-center lg:pt-10 lg:px-12">
+        <h1 class="text-2xl font-semibold tracking-tight w-10/12 mx-auto leading-none text-gray-900 md:text-6xl dark:text-white">
+          Enjoy the Experience of Managing HR & Payroll at your Fingertips.
+        </h1>
+        <p class="my-5 text-lg font-normal text-gray-500 md:text-xl md:w-6/12 mx-auto dark:text-gray-400">
+          Paie lets you easily manage your employees' payroll, benefits, expenses, devices, apps & more-in one place.
+        </p>
+        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+          <a href="#" class="inline-flex justify-center items-center py-4 px-8 text-base font-medium text-center text-white bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+            Try 14 Days for free
+          </a>
+          <a href="#" class="inline-flex justify-center items-center py-4 px-8 text-base font-medium text-center text-black bg-none focus:ring-4 focus:ring-gray-300 border-2 border-black dark:focus:ring-black">
+            Book a Demo
+          </a>
+        </div>
+      </div>
+    </section>
+
+    <div class="hidden md:flex">
+      <img src="https://paie-home-amt.netlify.app/_nuxt/dashboard.1d4bae62.png" alt="dash">
+    </div>
+  </div>
+  <div class="bg-[#F7C343] py-20">
+    <section class="grid md:grid-cols-2 w-11/12 gap-5 mx-auto items-center">
+      <h2 class="text-2xl md:text-4xl font-semibold md:w-9/12 text-gray-900">
+        Speed up payroll processing with other companies
+      </h2>
+      <div>
+        <img src="https://paie-home-amt.netlify.app/_nuxt/brand.2ea6d032.png" alt="brand">
+      </div>
+    </section>
+  </div>
+
+
+  <div class="bg-[#FEF9EC] py-10 md:py-20">
+    <h2 class="text-center text-2xl font-bold md:text-4xl">
+      Payroll so powerful it can run itself
+    </h2>
+    <p class="text-lg text-center text-gray-600 md:w-5/12 mx-auto mt-5">
+      Paie syncs all your business's HR data with payroll, so you never have to use a calculator or manually enter data, like hours and deductions.
+    </p>
+    <section class="w-11/12 mx-auto my-20 md:my-36 grid md:grid-cols-2 gap-5 items-center">
+      <div>
+        <img class="rounded-lg" src="https://paie-home-amt.netlify.app/_nuxt/dashboard2.36ee3025.png" alt="dash-one">
+      </div>
+      <div class="md:px-28">
+        <img class="w-12" src="https://paie-home-amt.netlify.app/_nuxt/one.1dd4ef10.png" alt="one">
+        <h5 class="text-2xl font-bold my-3">Payroll List</h5>
+        <p class="text-gray-600 text-lg font-semibold">
+          Pay employees accurately, on time, every pay run. Once your employee data is in our system, Rippling takes care of the rest.
+        </p>
+      </div>
+    </section>
+
+    <section class="w-11/12 mx-auto my-20 md:my-36 grid md:grid-cols-2 gap-5 items-center">
+      <div class="md:hidden">
+        <img class="rounded-lg" src="https://paie-home-amt.netlify.app/_nuxt/dashboard3.da435728.jpg" alt="dash-two">
+      </div>
+      <div class="md:px-28">
+        <img class="w-12" src="https://paie-home-amt.netlify.app/_nuxt/two.0eae59b6.png" alt="two">
+        <h5 class="text-2xl font-bold my-3">
+          Run Payroll in 90 Seconds
+        </h5>
+        <p class="text-gray-600 text-lg font-semibold">
+          Pay employees accurately, on time, every pay run. Once your employee data is in our system, Rippling takes care of the rest.
+        </p>
+      </div>
+      <div class="md:flex hidden">
+        <img src="https://paie-home-amt.netlify.app/_nuxt/dashboard3.da435728.jpg" alt="dash-two">
+      </div>
+    </section>
+
+    <section class="w-11/12 mx-auto my-20 md:my-36 grid md:grid-cols-2 gap-5 items-center">
+      <div>
+        <img class="rounded-lg" src="https://paie-home-amt.netlify.app/_nuxt/dashboard4.3a6c8185.png" alt="dash-three">
+      </div>
+      <div class="md:px-28">
+        <img class="w-12" src="https://paie-home-amt.netlify.app/_nuxt/three.0673b0b4.png" alt="three">
+        <h5 class="text-2xl font-bold my-3">Review Payroll</h5>
+        <p class="text-gray-600 text-lg font-semibold">
+          Pay employees accurately, on time, every pay run. Once your employee data is in our system, Rippling takes care of the rest.
+        </p>
+      </div>
+    </section>
+
+    <section class="w-11/12 mx-auto my-20 md:my-36 grid md:grid-cols-2 gap-5 items-center">
+      <div class="md:hidden">
+        <img class="rounded-lg" src="https://paie-home-amt.netlify.app/_nuxt/dashboard5.89b6ba5a.jpg" alt="dash-four">
+      </div>
+      <div class="md:px-28">
+        <img class="w-12" src="https://paie-home-amt.netlify.app/_nuxt/four.33a1b7eb.png" alt="four">
+        <h5 class="text-2xl font-bold my-3"> Payroll Report</h5>
+        <p class="text-gray-600 text-lg font-semibold">
+          Pay employees accurately, on time, every pay run. Once your employee data is in our system, Rippling takes care of the rest.
+        </p>
+      </div>
+      <div class="md:flex hidden">
+        <img class="rounded-lg" src="https://paie-home-amt.netlify.app/_nuxt/dashboard5.89b6ba5a.jpg" alt="dash-four">
+      </div>
+    </section>
+  </div>
+
+  <div class="w-11/12 mx-auto py-10 md:py-20">
+    <h2 class="text-2xl md:text-4xl font-bold md:w-5/12">
+      HRIS Technology that benefits present & future
+    </h2>
+    <section class="grid grid-cols-3 md:grid-col-3 mt-16 gap-5">
+      <div class="p-4">
+        <img src="https://paie-home-amt.netlify.app/_nuxt/handtime.fc8c6ef5.png" alt="handtime">
+        <h5 class="text-2xl font-bold my-2">Save Operational Costs</h5>
+        <p class="text-sm text-gray-600">
+          With a paperless HRIS system, the operational costs incurred by conventional HR activities can be more economical.
+        </p>
+        <div class="w-8/12 ml-1 my-5">
+          <hr>
+        </div>
+        <h2 class="text-5xl font-bold">+40%</h2>
+        <p class="text-lg text-gray-800 font-semibold mt-5 border-b-2 border-gray-700 border-dotted w-fit">Budget Saving</p>
+      </div>
+
+      <div class="p-4">
+        <img src="https://paie-home-amt.netlify.app/_nuxt/paper.a7ff3639.png" alt="paper">
+        <h5 class="text-2xl font-bold my-2">Organized HR Data</h5>
+        <p class="text-sm text-gray-600">
+          Company owners can access all employee data, HR activities and all reports in an attractive graphical analysis view.
+        </p>
+        <div class="w-8/12 ml-1 my-5">
+          <hr>
+        </div>
+        <h2 class="text-5xl font-bold">+35%</h2>
+        <p class="text-lg text-gray-800 font-semibold mt-5 border-b-2 border-gray-700 border-dotted w-fit">Data Accuracy</p>
+      </div>
+
+      <div class="p-4">
+        <img src="https://paie-home-amt.netlify.app/_nuxt/mobile.bd865e75.png" alt="mobile">
+        <h5 class="text-2xl font-bold my-2">Accurate & Fast Process</h5>
+        <p class="text-sm text-gray-600">
+          The Paie finance system is provides us the accurate data when reports for payroll and claims made by employees.
+        </p>
+        <div class="w-8/12 ml-1 my-5">
+          <hr>
+        </div>
+        <h2 class="text-5xl font-bold">+70%</h2>
+        <p class="text-lg text-gray-800 font-semibold mt-5 border-b-2 border-gray-700 border-dotted w-fit">Claim Efficiency</p>
+      </div>
+    </section>
+  </div>
+
+
+  <div class="bg-[#FEF9EC] py-10 md:py-20">
+    <section class="w-11/12 mx-auto grid grid-cols-1 md:grid-cols-2 gap-5 items-center">
+      <div>
+        <div class="flex md:hidden mb-4">
+          <img src="https://paie-home-amt.netlify.app/_nuxt/ok.9945c789.png" alt="okay">
+        </div>
+        <h2 class="text-3xl md:text-5xl font-bold">
+          Why our customers think we're the best
+        </h2>
+        <div class="md:flex hidden mt-4">
+          <img src="https://paie-home-amt.netlify.app/_nuxt/ok.9945c789.png" alt="okay">
+        </div>
+      </div>
+
+      <div>
+        <div class="bg-white p-5 rounded-shadow-md mb-5">
+          <div class="flex justify-between items-center">
+            <div class="flex items-center">
+              <img class="w-12 h-12 rounded-full mr-2" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="profile pic">
+              <div>
+                <h6 class="text-gray-800 font-bold">Delbert Dicki</h6>
+                <p class="text-gray-600">
+                  HR Officer
+                  <span class="hidden md:inline-block">at mailchimp</span>
+                </p>
+              </div>
+            </div>
+            <img class="md:w-40 w-28" src="https://paie-home-amt.netlify.app/_nuxt/mailchimp.9f63b97f.png" alt="mailchimp">
+          </div>
+          <hr class="my-5">
+          <p class="text-gray-600">
+            "My favorite thing about Paie is the compliance aspect. They make quarterly taxes, onboarding, and everything else so simple and easy, which saves me a ton of time!"
+          </p>
+        </div>
+
+        <div class="bg-white p-5 rounded-shadow-md mb-5">
+          <div class="flex justify-between items-center">
+            <div class="flex items-center">
+              <img class="w-12 h-12 rounded-full mr-2" src="https://flowbite.com/docs/images/people/profile-picture-1.jpg" alt="profile pic">
+              <div>
+                <h6 class="text-gray-800 font-bold">Chatalyne Devan</h6>
+                <p class="text-gray-600">
+                  HR Officer
+                  <span class="hidden md:inline-block">at DocuSign</span>
+                </p>
+              </div>
+            </div>
+            <img class="md:w-40 w-28" src="https://paie-home-amt.netlify.app/_nuxt/docusing.e4c26f19.png" alt="docusign">
+          </div>
+          <hr class="my-5">
+          <p class="text-gray-600">
+            "As soon as I saw how easy it was to set everything up I liked it, but the first time I ever dealt with customer service is when I really knew we chose the right payroll company."
+          </p>
+        </div>
+
+        <div class="bg-white p-5 rounded-shadow-md mb-5">
+          <div class="flex justify-between items-center">
+            <div class="flex items-center">
+              <img class="w-12 h-12 rounded-full mr-2" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="profile pic">
+              <div>
+                <h6 class="text-gray-800 font-bold">Marshall Beer</h6>
+                <p class="text-gray-600">
+                  HR Officer
+                  <span class="hidden md:inline-block">at Basecamp</span>
+                </p>
+              </div>
+            </div>
+            <img class="md:w-40 w-28" src="https://paie-home-amt.netlify.app/_nuxt/basecamp.1837979a.png" alt="basecamp">
+          </div>
+          <hr class="my-5">
+          <p class="text-gray-600">
+            "Paie helped me a lot especially with the attendance, so now the marketing team can check in and leave with the offsite application. The payroll process is also very fast!"
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
+
+<div class="bg-[#F7C343] py-10 md:py-20">
+<section class="w-11/12 mx-auto">
+        <h2 class="text-3xl md:text-5xl font-bold md:w-7/12">Start enjoying the best experience in managing HR with PAIE</h2>
+        <p class="text-lg text-gray-800 my-5 md:w-5/12">Learn how Paie can help you effortlessly onboard and manage your employess, whether you have a workforce of 2 or 2,000.</p>
+        <div class="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
+            <a href="#" class="inline-flex justify-center items-center py-4 px-8 text-base font-medium text-center text-white bg-black hover:bg-black-600 focus:ring-4 focus:ring-black">
+                Try 14 Days for free
+            </a>
+            <a href="#" class="inline-flex justify-center items-center py-4 px-8 text-base font-medium text-center text-black bg-none focus:ring-4 focus:ring-gray-300 border-2 border-black dark:focus:ring-black">
+                Book a Demo
+            </a>  
+        </div>
+</section>
+</div>
+</div>
+
+</template>
+
+<script>
+    
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+   <Navbar/>
+   <Main/>
+   <Footer/>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
